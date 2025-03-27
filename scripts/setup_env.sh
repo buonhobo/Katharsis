f7e0e4b5361f7fa10c84b2ba7dd34d0f05e9685f
buonhobo <bnnalx@gmail.com>
2025-03-27 18:13:41 +0100

    Update Flatpak configuration and dependencies; remove unused JSON files

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,3 +1,5 @@
-#This is enough in fedora 41
-#If creating a venv then you should pass --system-site-packages
-pip install kathara[pyuv]
+# mypy shouldn't be necessary, but I keep it in the dependencies because otherwise the flatpak build fails...
+pip install kathara mypy
+pip freeze > src/requirements.txt
+# This flatpak pip generator is not the original one, I emptied the system_packages array to make it work.
+python scripts/flatpak-pip-generator.py --runtime 'runtime/org.gnome.Sdk/x86_64/48' --output flatpak/pypi-dependencies -r src/requirements.txt --ignore-installed Pygments,six
