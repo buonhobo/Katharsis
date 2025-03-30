514373f0d43c641249b2b31936fe8e00b7817db1
buonhobo <bnnalx@gmail.com>
2025-03-30 13:37:20 +0200

    added pyuv dependency

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,5 +1,5 @@
 # mypy shouldn't be necessary, but I keep it in the dependencies because otherwise the flatpak build fails...
-pip install kathara mypy
+pip install kathara mypy pyuv requirements-parser
 pip freeze > src/requirements.txt
 # This flatpak pip generator is not the original one, I emptied the system_packages array to make it work.
 python scripts/flatpak-pip-generator.py --runtime 'runtime/org.gnome.Sdk/x86_64/48' --output flatpak/pypi-dependencies -r src/requirements.txt --ignore-installed Pygments,six
